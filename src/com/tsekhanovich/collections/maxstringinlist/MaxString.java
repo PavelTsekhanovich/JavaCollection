a0fc3c182c4ed2cd10c295f7a8a0a99cc71c7ca7
Pavel Tsekhanovich <p.tsekhanovich93@gmail.com>
2018-10-25 17:56:01 +0300

    Find max string

---
+++ src/com/tsekhanovich/collections/maxstringinlist/MaxString.java
@@ -0,0 +1,34 @@
+package com.tsekhanovich.collections.maxstringinlist;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author Pavel Tsekhanovich 25/10/2018
+ *
+ * On static List<String> changeList(List<String> list) method you must do:
+ * - find the longest string in list
+ * -if you use method max of class Collections : don't forget to implement a comparator for this method
+ *    Collections.max(list, (s1,s2)->{...})
+ *    by default, this method compares strings alphabetically!
+ * - of course you can use other approaches(for example comparison in a for-loop)
+ * - replace all list items with this row
+ * - return modified list
+ */
+
+public class MaxString {
+
+    public static void main(String[] args) {
+        changeList(Arrays.asList("hi", "hello", "goodmorning", "com"))
+                .forEach(System.out::println);
+    }
+
+    public static List<String> changeList(List<String> list) {
+        return list.stream()
+                .map(s -> s = Collections.max(list, Comparator.comparingInt(String::length)))
+                .collect(Collectors.toList());
+    }
+}
