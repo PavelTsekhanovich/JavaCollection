55f520a5a899b4f1d0f2df7bcb45b1a6555ccd27
Pavel Tsekhanovich <p.tsekhanovich93@gmail.com>
2018-10-26 15:33:43 +0300

    Spliterator

---
+++ src/com/tsekhanovich/collections/spliteratorsqrt/SpliteratorSqrt.java
@@ -0,0 +1,44 @@
+package com.tsekhanovich.collections.spliteratorsqrt;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author Pavel Tsekhanovich 26/10/2018
+ *
+ * On spliteratorWork( List<Double> lst ):
+ * - using Spliterator add to List<Double> sqrts all square roots of items from lst ,if this item greater than 1
+ * - use Math.sqrt(number) to find square root : Math.sqrt(4)-> 2.0
+ * On print(List<Double> list):
+ * - get spliterator() of given list
+ * - divide spliterator into 2 parts ( use spliterator.trySplit())
+ * - print all items which >=2 from first part (use .println())
+ * - print empty string
+ * - print all items which >=10 from second part (use .println())
+ *
+ * 4 4 4 100 100 100
+ */
+
+public class SpliteratorSqrt {
+
+    public static void main(String[] args) {
+        print(spliteratorWork(Arrays.asList(4.0, 4.0, 4.0, 100.0, 100.0, 100.0)));
+    }
+
+    private static List<Double> spliteratorWork(List<Double> lst) {
+        List<Double> sqrs = new ArrayList<>();
+        lst.spliterator().forEachRemaining(e -> sqrs.add(Math.sqrt(e)));
+        return sqrs;
+    }
+
+    private static void print(List<Double> list) {
+        list.spliterator().trySplit().forEachRemaining(e -> {
+            if (e >= 2) System.out.println(e);
+        });
+        System.out.println();
+        list.spliterator().forEachRemaining(e -> {
+            if (e >= 10) System.out.println(e);
+        });
+    }
+}
